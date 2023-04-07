cb904e48e2c15211ecdaefd2ddbf6c9258578918
Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
2023-04-07 15:19:58 +0530

    sort by present or absence

--- app/src/main/java/com/example/iskcon/AttendanceRecordActivity.kt
+++ app/src/main/java/com/example/iskcon/AttendanceRecordActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.airbnb.epoxy.EpoxyRecyclerView
@@ -67,16 +68,71 @@ class AttendanceRecordActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         isPresent.adapter = adapter
         isPresent.setSelection(0)
+        isPresent.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // Handle the selection event here
+                if (isDateSelected()){
+                    val selectedItem = parent?.getItemAtPosition(position) as String
+                    val date=selectDate.selectedItem.toString()
+                    if (position != 0){
+                        val isPresent= selectedItem=="Present"
+                        getStudentsPA(date,isPresent)
+//                    val dataList=ArrayList<AttendanceRecord>()
+//                    dataList.clear()
+//                    dataList.add(AttendanceRecord("9370868033","Sanjay",true))
+//                    controller.setData(dataList)
+                    }
+                }
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle the case where no item is selected
+            }
+        }
 
 
 
-
+    }
+    fun isDateSelected():Boolean{
+        if (selectDate.selectedItem.toString()=="Select Date"){
+            (selectDate.selectedView as? TextView)?.error="Select Date"
+            return false
+        }
+        return true
     }
     fun getStudentsList(date:String){
         val dataList=ArrayList<AttendanceRecord>()
         dataList.clear()
         Log.i("fgds",date)
         databaseReference.child(date)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (document in snapshot.children){
+                        val studentNo=document.child("studentNo").getValue(String::class.java).toString()
+                        val studentName=document.child("studentName").getValue(String::class.java).toString()
+                        val isPresent= document.child("present").getValue(Boolean::class.java)
+                        isPresent?.let { AttendanceRecord(studentNo,studentName, it) }
+                            ?.let { dataList.add(it) }
+                    }
+                    controller.setData(dataList!!)                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+            })
+    }
+    fun getStudentsPA(date:String,isPresent:Boolean){
+        val dataList=ArrayList<AttendanceRecord>()
+        dataList.clear()
+        Log.i("fgds",date)
+        databaseReference.child(date)
+            .orderByChild("present").equalTo(isPresent)
             .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     for (document in snapshot.children){
