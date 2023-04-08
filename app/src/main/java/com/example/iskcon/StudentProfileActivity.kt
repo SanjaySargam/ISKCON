2ee68eb796669e34714e2c913e38244fb3b6e224
Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
2023-04-08 16:54:55 +0530

    call by attendance done

--- app/src/main/java/com/example/iskcon/StudentProfileActivity.kt
+++ app/src/main/java/com/example/iskcon/StudentProfileActivity.kt
@@ -2,6 +2,9 @@ package com.example.iskcon
 
 import android.app.Dialog
 import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -9,6 +12,8 @@ import android.util.Log
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.iskcon.databinding.ActivityMainBinding
 import com.example.iskcon.databinding.ActivityStudentProfileBinding
 
@@ -31,6 +36,31 @@ class StudentProfileActivity : AppCompatActivity() {
         dialogText = progressDialog!!.findViewById(R.id.dialog_text)
         dialogText!!.setText("Loading")
         getDetails(phone)
+
+        binding.number.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                // You already have permission, so call the phone number
+                val phoneNumber = binding.number.text.toString() // Replace with the phone number you want to call
+                val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:$phoneNumber"))
+                startActivity(intent)
+            } else {
+                // You don't have permission, so request it
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), REQUEST_CALL_PHONE_PERMISSION)
+            }
+
+        }
+        binding.number2.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                // You already have permission, so call the phone number
+                val phoneNumber = binding.number2.text.toString() // Replace with the phone number you want to call
+                val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:$phoneNumber"))
+                startActivity(intent)
+            } else {
+                // You don't have permission, so request it
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), REQUEST_CALL_PHONE_PERMISSION)
+            }
+
+        }
     }
 
     fun getDetails(no: String) {
@@ -65,5 +95,8 @@ class StudentProfileActivity : AppCompatActivity() {
             }
 
         })
+    }
+    companion object {
+        private const val REQUEST_CALL_PHONE_PERMISSION = 1
     }
 }
