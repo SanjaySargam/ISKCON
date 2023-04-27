0566c84a4584956e0b669de57159d8f1f397bb47
Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
2023-04-27 19:09:41 +0530

    Connected Firebase TOTAL_STUDENTS data to layout

--- app/src/main/java/com/example/iskcon/MainActivity.kt
+++ app/src/main/java/com/example/iskcon/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.iskcon
 
+import android.app.Dialog
+import android.content.ContentValues
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -31,20 +36,51 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var epoxyControllerCategory: CategoryController
     val categories = ArrayList<String>()
+    private var progressDialog: Dialog? = null
+    private var dialogText: TextView? = null
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        progressDialog = Dialog(this)
+        progressDialog!!.setContentView(R.layout.dialog_layout)
+        progressDialog!!.setCancelable(false)
+        progressDialog!!.getWindow()!!
+            .setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        dialogText = progressDialog!!.findViewById(R.id.dialog_text)
+        dialogText!!.setText("Loading")
         FirebaseQuery.firestore = FirebaseFirestore.getInstance()
         categories.add("New Student")
         categories.add("Take Attendance")
         categories.add("Attendance Record")
         categories.add("Feedback")
         setRecyclerView()
+        getTotalStudents()
     }
+    fun getTotalStudents(){
+        progressDialog!!.show()
+        val callback = object : FirebaseQuery.FirestoreCallback3 {
+            override fun onDataReceived(data: Int) {
+                // Do something with the data
+                Log.d(ContentValues.TAG, "Data received: $data")
+                binding.count.text=data.toString()
+            }
+        }
+        FirebaseQuery.getTotal(callback,object : MyCompleteListener{
+            override fun onSuccess() {
+                progressDialog!!.dismiss()
+            }
 
+            override fun onFailure() {
+                progressDialog!!.dismiss()
+            }
+
+        })
+    }
     fun setRecyclerView() {
         binding.categoryList.apply {
             layoutManager = LinearLayoutManager(context)
